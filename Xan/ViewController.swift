034b050409191b3ec0f78afb50c04ca7e64d2380
Marcus Seel <marcus.seel@softwareinmotion.de>
2015-02-24 08:30:54 +0100

    fixes missing files

--- Xan/ViewController.swift
+++ Xan/ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var nrfManager: NRFManager!
+    var timer = dispatch_source_t()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        nrfManager = NRFManager(
+            onConnect: {
+                println("Connected")
+            },
+            onDisconnect: {
+                println("Disconnected")
+            },
+            onData: {
+                (data:NSData?, string:String?)->() in
+                println("Received data - String: \(string) - Data: \(data)")
+            }
+        )
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +36,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func moveBackward(sender: AnyObject) {
+        sendData("2")
+    }
+    
+    @IBAction func moveLeft(sender: AnyObject) {
+        sendData("3")
+    }
+    
+    @IBAction func startForward(sender: AnyObject) {
+        sendData("1")
+    }
+    
+    @IBAction func stopMoving(sender: AnyObject) {
+        sendData("5")
+    }
+    
+    @IBAction func moveRight(sender: AnyObject) {
+        sendData("4")
+    }
+
+    func sendData(direction: String)
+    {
+        let result = self.nrfManager.writeString(direction)
+    }
 
 }
 
